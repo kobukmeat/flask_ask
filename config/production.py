42fc0745206f0033bf33b3df943e66390828f64a
kobukmeat <kevinjjang08@gmail.com>
2024-11-07 00:23:30 +0900

    edit

--- config/production.py
+++ config/production.py
@@ -7,26 +7,3 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xc9k\xdcUe\xc1Q\xdafR\xad\xa5\x87]\xf8\xf0!\x98\x06q'
 
-
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-        }
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'default',
-        },
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['file']
-    }
-})
